c92454f286902e56b2aab625d3d83bece9ed7e54
Zach <zacbenincasa@gamil.com>
2022-07-09 16:12:19 -0600

    added in the while and for loops

---
+++ W/js/app.js
@@ -0,0 +1,9 @@
+var user_names= ["Tim","jim", "kim", "finn","Apple","Dew", "Slagathor", "vick", "Brohiem","john"];
+var user_age = [19,29,14,57,34,54,28,10,100,52]
+
+
+while(user_age >=18){
+    console.log(user_names)
+    counter=counter+1
+}
+if(user_age>18, console.log(user_name))
