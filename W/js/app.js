2f3f73716c5b06446858f0c7e0dda237db0cb196
Zach <zacbenincasa@gamil.com>
2022-07-09 16:14:11 -0600

    added more

--- W/js/app.js
+++ W/js/app.js
@@ -5,5 +5,5 @@ var user_age = [19,29,14,57,34,54,28,10,100,52]
 while(user_age >=18){
     console.log(user_names)
     counter=counter+1
-}
-if(user_age>18, console.log(user_name))
+};
+if(user_age>18, console.log(user_name));
